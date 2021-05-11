48bffe58f523cd0c22b0171c9372ecc063991dd4
Diya-Dangol <diya.dangol15@gmail.com>
2021-05-11 10:16:38 +0545

    Updates

---
+++ precache-manifest.c0e09d4883d8f03fbeb3564f61ca0fbc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa95bed43ba885d758ed5b3e9c4b73d0",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "b804794580c854352f69",
+    "url": "/tictactoe/static/js/2.38416e9a.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/tictactoe/static/js/2.38416e9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b6864bc50f28e70085c",
+    "url": "/tictactoe/static/js/main.990fcee6.chunk.js"
+  },
+  {
+    "revision": "8fd1009f88de2a8f7d1e",
+    "url": "/tictactoe/static/js/runtime-main.57baa19c.js"
+  }
+]);
